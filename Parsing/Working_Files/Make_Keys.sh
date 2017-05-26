b57f9ef58f9e6ab6f451921a9e716340aa8d083c
Schuyler Smith <schuyler@jareds-air2.student.iastate.edu>
2017-05-26 12:56:39 -0500

    first commit

--- Parsing/Working_Files/Make_Keys.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir Keys
-
-while read f; do
-	echo $f | awk '{print $2}' | tr ',' '\n' > Keys/`echo $f | awk '{print $1}'`.list
-done < AR_DB_Classes
-
-cat AR_DB_Classes | awk '{print $1}' > Classes.list
